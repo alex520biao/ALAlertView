09f6a4ebdaacc8511d337a7c158cb527511b8080
alex520biao <alex520biao@163.com>
2015-10-13 21:10:09 +0800

    update

---
+++ Pod/Classes/ALAlertView.h
@@ -0,0 +1,13 @@
+//
+//  ALAlertView.h
+//  Pods
+//
+//  Created by liubiao on 15/10/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAlertView : UIView
+
+@end
---
+++ Pod/Classes/ALAlertView.m
@@ -0,0 +1,21 @@
+//
+//  ALAlertView.m
+//  Pods
+//
+//  Created by liubiao on 15/10/13.
+//
+//
+
+#import "ALAlertView.h"
+
+@implementation ALAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
